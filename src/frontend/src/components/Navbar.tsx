80a0dcbcb459bf80ffdf939cb56546b1998903e4
tommy <tommypoon36@gmail.com>
2021-04-03 16:02:40 -0400

    created navbar component with button to request localhost backend

--- src/frontend/src/components/Navbar.tsx
+++ src/frontend/src/components/Navbar.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
+import axios from 'axios';
 
 const Nav = () => {
+  const logout = async () => {
+    const response = await axios.post('http://localhost:8000/api/logout')
+    console.log(response.data);
+  }
+
   return (
     <nav className="navbar navbar-expand-md navbar-dark bg-dark mb-4">
       <div className="container-fluid">
         <div>
           <ul className="navbar-nav me-auto mb-2 mb-md-0">
             <li className="nav-item">
-              <a className="nav-link active" onClick={() => console.log("Clicked")}>Click</a>
+              <a className="nav-link active" onClick={logout}>Click</a>
             </li>
           </ul>
         </div>
